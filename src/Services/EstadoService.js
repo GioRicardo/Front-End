73f1caabb0cb3d3c9ed22fb5124a7ffcd9a719eb
GioRicardo <giordanfuerzagrp@gmail.com>
2023-04-11 18:35:58 -0500

    actualizacion

---
+++ src/Services/EstadoService.js
@@ -0,0 +1,45 @@
+import { axiosConfig } from "../Configuration/AxiosConfig";
+
+// obtener los tipos de equipos
+const getEstados = (estado) => {
+    return axiosConfig.get('estadoequipos?estado='+estado, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+
+// crear estado equipo
+const createEstado = (data = {}) => {
+    return axiosConfig.post('estados', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+const editarEstado = (tipoId, data) => {
+  return axiosConfig.put(`estadoequipos?id=${tipoId}`, data, {
+   headers: {
+      'Content-type': 'application/json'
+   }
+  });
+}
+
+// opcional
+const borrarTipoEquipo = (tipoId) => {
+  return axiosConfig.delete(`tipoequipos/${tipoId}`, {}, {
+   headers: {
+      'Content-type': 'application/json'
+   }
+  });
+}
+
+
+export {
+    getEstados,
+    createEstado,
+    editarEstado,
+    borrarTipoEquipo
+}
